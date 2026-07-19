bb1843b37f14b23af0197432086352396e2c0b95
FatimahHaruna <Fatimaharuna2007@gmail.com>
2026-07-19 17:54:08 +0100

    Styling game

---
+++ RockPaperScissorsGame/rockpaperscissors.js
@@ -0,0 +1,41 @@
+let wins = 0;
+let losses = 0;
+let draws = 0;
+function myFunction(playerMove) {
+    let randomNum = Math.random();
+    let computerMove = ' ';
+    let status = ' ';
+     //Computer Move
+    if(randomNum >= 0 && randomNum < 1/3) {
+        computerMove = 'Rock';
+    }
+    else if(randomNum >= 1/3 && randomNum < 2/3) {
+        computerMove = 'Paper';
+    }
+    else if(randomNum >= 2/3 && randomNum <= 1) {
+        computerMove = 'Scissors';
+    }
+    //Compare with Player Move
+    if(playerMove == 'Rock' && computerMove == 'Paper' || playerMove == 'Paper' && computerMove == 'Scissors' || playerMove == 'Scissors' && computerMove == 'Rock') {
+        status = 'You lost!'
+        losses++
+    }
+    else if(playerMove == 'Rock' && computerMove == 'Scissors' || playerMove == 'Paper' && computerMove == 'Rock' || playerMove == 'Scissors' && computerMove == 'Paper') {
+        status = 'You won!' 
+        wins++
+    }
+    else if(playerMove == 'Rock' && computerMove == 'Rock' || playerMove == 'Paper' && computerMove == 'Paper' || playerMove == 'Scissors' && computerMove == 'Scissors') {
+        status = 'It\'s a tie!'
+        draws++
+    }
+    //Display status
+    document.getElementById('status').innerHTML = status;
+    document.getElementById('scoreboard').innerHTML = `Wins: ${wins}, Draws: ${draws}, Losses: ${losses}`;
+}
+function resetScore() {
+    wins = 0;
+    losses = 0;
+    draws = 0;
+    document.getElementById('status').innerHTML = 'Score reset.';
+    document.getElementById('scoreboard').innerHTML = `Wins: ${wins}, Draws: ${draws}, Losses: ${losses}`;
+}
